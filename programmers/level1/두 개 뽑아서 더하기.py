c600e24780c13e03bba18ceae38bcaab863d3adc
sieun <31584255+sileeee@users.noreply.github.com>
2022-03-08 22:55:15 +0900

    Create 두 개 뽑아서 더하기.py

---
+++ programmers/level1/두 개 뽑아서 더하기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = []
+    s = set()
+    
+    for i in range(len(numbers)-1):
+        for j in range(i+1, len(numbers)):
+            s.add(numbers[i]+numbers[j])
+    answer = sorted(s)
+    
+    return answer
