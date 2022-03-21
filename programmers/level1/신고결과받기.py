c092e82b69c8638728695efd1cec40b3f60a60d9
sieun <31584255+sileeee@users.noreply.github.com>
2022-03-21 23:51:22 +0900

    Update 신고결과받기.py

--- programmers/level1/신고결과받기.py
+++ programmers/level1/신고결과받기.py
@@ -1,26 +1,24 @@
 def solution(id_list, report, k):
-    dict_report = {name: [] for name in id_list}
-    dict_count = {name: 0 for name in id_list}
-    stop = []
-    result = []
+    answer = [0]*len(id_list)
+    table = []
+    for i in range(len(report)):
+        table.append([])
+    table2 = [0]*len(id_list)
+    table3 = []
 
-    for i in report:
-        user, get_report_user = i.split()
-        if get_report_user not in dict_report[user]:
-            dict_report[user].append(get_report_user)
-
-    for i in id_list:
-        for j in dict_report:
-            if i in dict_report[j]:
-                dict_count[i] += 1
-        if dict_count[i] >= k:
-            stop.append(i)
-
-    for i in dict_report:
-        count = 0
-        for j in dict_report[i]:
-            if j in stop:
-                count += 1
-        result.append(count)
-
-    return result
+    s = set(report)
+    for i in s:
+        first, last = i.split()
+        index = id_list.index(first)
+        table[index].append(last)
+        index2 = id_list.index(last)
+        table2[index2] +=1
+        if(table2[id_list.index(last)] == k):
+            table3.append(last)
+    for j in range(len(table)):
+        if(table[j] == []):
+            continue
+        for w in table3:
+            if w in table[j]:
+                answer[j] +=1
+    return answer
