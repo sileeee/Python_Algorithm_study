3891c19859fe47130860335e8eb2876603718517
sieun <31584255+sileeee@users.noreply.github.com>
2022-03-23 22:36:48 +0900

    Update 숫자 문자열과 영단어.py

--- programmers/level1/숫자 문자열과 영단어.py
+++ programmers/level1/숫자 문자열과 영단어.py
@@ -1,17 +1,18 @@
 def solution(s):
+    answer = ""
     eng = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-
-    if s.isdigit(): 
-        return int(s) 
-    answer = ''
-    temp = ''
-    for i in s:
-        if i.isdigit():
-            answer+=i
+    i = 0
+    while(i<len(s)):
+        if ord(s[i]) > 64:
+            for j in range(3,6):
+                a = s[i:i+j]
+                if a in eng:
+                    answer += str(eng.index(a))
+                    i += j
+                    break
+                else : 
+                  continue
         else:
-            temp += i
-            if temp in eng:
-                answer += str(eng.index(temp))
-                temp = ''
- 
+            answer += s[i]
+            i +=1
     return int(answer)
