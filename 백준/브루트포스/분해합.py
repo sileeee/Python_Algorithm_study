93c1b198254e5fb438dc9274099bed3df8826706
sieun <31584255+sileeee@users.noreply.github.com>
2022-04-07 00:32:08 +0900

    Update 분해합.py

--- 백준/브루트포스/분해합.py
+++ 백준/브루트포스/분해합.py
@@ -1,10 +1,19 @@
-n = int(input())
+N = int(input())
+length = 0
 
-for i in range(1, n+1):  
-    num = sum((map(int, str(i))))  
-    num_sum = i + num 
-    if num_sum == n:
-        print(i)
-        break
-    if i == n:  
-        print(0)
+tmp = N
+while(tmp>0): # N의 자릿수를 구한다
+  tmp //= 10
+  length +=1
+
+for i in range(1, N+1):
+  result = i
+  tmp = i
+  while(tmp>0):
+    result += tmp%10
+    tmp //= 10
+  if(result == N):
+    print(i)
+    break
+  if(i == N):
+    print(0)
