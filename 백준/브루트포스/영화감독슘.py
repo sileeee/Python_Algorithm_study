f57b26b9ca58dc01a1e3c0f166b4d1387f100038
sieun <31584255+sileeee@users.noreply.github.com>
2022-04-13 23:37:02 +0900

    Update 영화감독슘.py

--- 백준/브루트포스/영화감독슘.py
+++ 백준/브루트포스/영화감독슘.py
@@ -1,9 +1,10 @@
-N = int(input())
-movie = 666
+x = int(input())
+i = int(666)
 
-while N:
-    if "666" in str(movie):
-        N -= 1
-    movie += 1
-
-print(movie - 1)
+while(1):
+  if '666' in str(i):
+    x -=1
+  if(x == 0):
+    break
+  i+=1
+print(i)
