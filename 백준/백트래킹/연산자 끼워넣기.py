ee92b4d01eb5838dcbce107922ed861756bc9c9f
sieun <31584255+sileeee@users.noreply.github.com>
2022-05-09 23:52:31 +0900

    Update 연산자 끼워넣기.py

--- 백준/백트래킹/연산자 끼워넣기.py
+++ 백준/백트래킹/연산자 끼워넣기.py
@@ -1,32 +1,31 @@
-# error
 import itertools
 
 n = int(input())
 num = list(map(int,input().split()))
 oper = list(map(int, input().split()))
-oper_arr = ['+','-','x','/']
+oper_list = ['+','-','x','/']
 permu_oper = []
-max_result = 0
-min_result = sum(num)
+maximum = -1e9
+minimum = 1e9
 
 for i in range(len(oper)):
     for j in range(oper[i]):
-        permu_oper.append(oper_arr[i])
+        permu_oper.append(oper_list[i])
 
 npm = list(itertools.permutations(permu_oper, len(permu_oper))) #연산자 순열
 
 for i in npm: 
     result = num[0]
-    for j in range(1, len(num)):
-        if (*i) == '+':
-            result += j
-        if (*i) == '-':
-            result -= j
-        if (*i) == 'x':
-            result *= j
-        if (*i) == '/':
-            result /= j
-    max_result = max(max_result, result)
-    min_result = min(min_result, result)
-print(max_result)
-print(min_result)
+    for j in range(1, n):
+        if i[j-1] == '+':
+            result += num[j]
+        elif i[j-1] == '-':
+            result -= num[j]
+        elif i[j-1] == 'x':
+            result *= num[j]
+        elif i[j-1] == '/':
+            result = int(result / num[j])
+    maximum = max(maximum, result)
+    minimum = min(minimum, result)
+print(maximum)
+print(minimum)
