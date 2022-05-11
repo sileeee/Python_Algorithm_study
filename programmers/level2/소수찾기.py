a12b847230631e2338e1b49e0a07cc1e3f8c07c3
sieun <31584255+sileeee@users.noreply.github.com>
2022-05-12 00:31:55 +0900

    Update 소수찾기.py

--- programmers/level2/소수찾기.py
+++ programmers/level2/소수찾기.py
@@ -1,24 +1,22 @@
-#error
 import itertools
 
 def solution(numbers):
     answer = 0
-    arr = []
-    
+    arr = set()
+    flag = True
+
     for i in range(1, len(numbers)+1):
-        per = itertools.permutations(numbers, i)
-        for j in per:
-            perStr = ''.join(j)
-            arr.append(perStr)
-    arr = set(arr)
-    
+        per = set(map(int, map(''.join, itertools.permutations(list(numbers), i))))
+        arr |= per # 합집합
+    arr -= set(range(0, 2))
+
     for i in arr:
-        if int(i) == 1:
-            continue
-        for j in range(2, int(i)):
-            if int(i)%j == 0:
-                continue
-        answer +=1
-               
-        
+        flag = True
+        for j in range(2, i):
+            if i%j == 0:
+                flag = False
+                break
+        if(flag == True):
+            answer +=1
+
     return answer
