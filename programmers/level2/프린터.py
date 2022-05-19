d03ea263113eb5549891064536c3511a907dc4e7
sieun <31584255+sileeee@users.noreply.github.com>
2022-05-19 16:23:29 +0900

    Update 프린터.py

--- programmers/level2/프린터.py
+++ programmers/level2/프린터.py
@@ -1,15 +1,25 @@
 def solution(priorities, location):
-  answer = 0
-  from collections import deque
-
-  d = deque([(v,i) for i,v in enumerate(priorities)])
-
-  while len(d):
-      item = d.popleft()
-      if d and max(d)[0] > item[0]:
-          d.append(item)
-      else:
-          answer += 1
-          if item[1] == location:
-              break
-  return answer
+    answer = 0
+    flag = False
+    idx = location
+    while priorities:
+        for i in range(1, len(priorities)):
+            flag = False
+            if priorities[0]<priorities[i]:
+                priorities.append(priorities[0])
+                priorities.pop(0)
+                if idx == 0:
+                    idx = len(priorities)-1
+                else:
+                    idx -=1
+                flag = True
+                break
+        if flag==False:
+            if idx == 0:
+                return answer+1
+            else:
+                priorities.pop(0)
+                idx -=1
+                answer+=1
+        
+    return answer
