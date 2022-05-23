461e4e8879bee5ff096b51d9b62cd1dcdf5bb7eb
sieun <31584255+sileeee@users.noreply.github.com>
2022-05-23 23:54:17 +0900

    Update 신나는 함수 실행.py

--- 백준/동적계획법/신나는 함수 실행.py
+++ 백준/동적계획법/신나는 함수 실행.py
@@ -1,32 +1,22 @@
-# error
-
-arr = []
-
-def find(a, b, c):
-    if((a, b, c)in arr):
-        idx = arr.index(a, b, c)
-        return arr[idx]
-    else:
-        arr.append((a, b, c))
-        return 0
+arr = [[[0]*(21) for _ in range(21)] for _ in range(21)]
 
 def w(a, b, c):
     answer = 0
-    value = find(a, b, c)
     if (a <= 0 or b <= 0 or c <= 0):
         return 1
     elif (a > 20 or b > 20 or c > 20):
         return w(20, 20, 20)
-    elif (a < b and b < c):
-        if (value):
-            return value
-        else:
-            w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+    elif arr[a][b][c]:
+        return arr[a][b][c]
+    elif (a < b < c):
+        arr[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return arr[a][b][c]
     else:
-        if (value):
-            return value
+        if arr[a][b][c]:
+            return arr[a][b][c]
         else:
-            w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+            arr[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+            return arr[a][b][c]
 
 while(1):
     a, b, c = map(int, input().split())
@@ -34,4 +24,4 @@ while(1):
         break
     else:
         answer = w(a, b, c)
-print("w({}, {}, {}) = {}".format(a, b, c, answer))
+    print("w({}, {}, {}) = {}".format(a, b, c, answer))
