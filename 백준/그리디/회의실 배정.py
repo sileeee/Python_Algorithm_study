fb89942e670e19658fd5feaacc3c2668016e8421
sieun <31584255+sileeee@users.noreply.github.com>
2022-06-28 23:33:39 +0900

    Create 회의실 배정.py

--- 백준/그리디/회의실 배정.py
+++ 백준/그리디/회의실 배정.py
@@ -1,16 +1,16 @@
-# error
-
 n = int(input())
-increase = [1 for _ in range(n)]
 arr = []
-line = []
 
 for _ in range(n):
     arr.append(list(map(int, input().split())))
-arr.sort()
+arr = sorted(arr, key = lambda x:(x[1],x[0]))
 
-for i in range(1, n):
-    for j in range(i):
-        if arr[j][1]<=arr[i][1] and arr[j][1]<=arr[i][0]:
-            increase[i] = max(increase[i], increase[j]+1)
-print(n-max(increase))
+start = 0
+end = 0
+result = 0
+for i in range(len(arr)):
+    if end<=arr[i][0]:
+        result += 1
+        end = arr[i][1]
+        
+print(result)
