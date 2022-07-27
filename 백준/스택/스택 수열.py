c65783d64a1ff3e71e2a466a6c9ab62d85e0688f
sieun <31584255+sileeee@users.noreply.github.com>
2022-07-28 00:22:18 +0900

    Update 스택 수열.py

--- 백준/스택/스택 수열.py
+++ 백준/스택/스택 수열.py
@@ -1,8 +1,11 @@
 # error
+
 n = int(input())
-arr = [x+1 for x in range(n)]
+arr = [x+1 for x in range(n-1, -1, -1)]
 answer = []
+result = []
 s = []
+j = 0
 
 for _ in range(n):
     answer.append(int(input()))
@@ -10,18 +13,26 @@ for _ in range(n):
 for i in answer:
         if len(s)!=0 and s[-1] == i:
             s.pop()
-            print("-")
+            result.append("-")
             continue
-        for j in arr: # 멈춘 인덱스 기록해야함
+        while(True):
+            if len(arr)!=0:
+                j = arr.pop() 
+            else: 
+                break
             if i != j and i > j:
                 s.append(j)
-                print("+")
+                result.append("+")
             elif i == j:
                 s.append(j)
+                result.append("+")
                 s.pop()
-                print("-")
+                result.append("-")
                 break
             elif i < j : 
                 print("NO")
 if len(s)!=0:
     print("NO")
+else:
+    for x in result:
+        print(x)
