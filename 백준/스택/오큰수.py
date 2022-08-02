947e6bbf4b3fdde46d0fb1031692adcba4ff711c
sieun <31584255+sileeee@users.noreply.github.com>
2022-08-02 23:05:02 +0900

    Update 오큰수.py

--- 백준/스택/오큰수.py
+++ 백준/스택/오큰수.py
@@ -1,15 +1,15 @@
-# 시간초과
-n = int(input())
-arr = list(map(int, input().split()))
-s = []
+import sys
 
-for i in range(len(arr)):
-    for j in range(i+1, len(arr)):
-        if arr[i]<arr[j]:
-            s.append(arr[j])
-            break
-        elif arr[i] >=arr[j] and j == len(arr)-1:
-            s.append(-1)
-s.append(-1)
-for i in s:
+n = int(input())
+arr = list(map(int, sys.stdin.readline().split()))
+s = [] # 인덱스 임시 저장소
+result = [-1] * n
+
+s.append(0)
+for i in range(1, n):
+    while s and arr[s[-1]] < arr[i]:
+        result[s.pop()] = arr[i] 
+    s.append(i)
+
+for i in result:
     print(i, end=' ')
